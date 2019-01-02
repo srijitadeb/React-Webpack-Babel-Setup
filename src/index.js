2fb3d4e88e3adbc4201703a3d25b64bba1e8f75a
Rimpo Dimbo <rimpo@Srijitas-MacBook-Air.local>
2019-01-02 22:21:16 +0530

    Remove the now ignored directory node_modules

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log(' React Webpack Babel Setup');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'React Webpack Babel Setup';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
